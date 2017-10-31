6b774ee0d2274007e62442a07174750fb6872406
benstoehr <benjaminxstoehr@gmail.com>
2017-10-31 13:31:40 -0500

    Works

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
+from __future__ import print_function # In python 2.7
+
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import make_response
+
+import sys
 
 app = Flask(__name__)
 
@@ -16,7 +20,7 @@ def hello(name=None):
 @app.route('/query/', methods=['GET','POST'])
 def query():
     if(request.method == 'POST'):
-        print(request.data)
+        print(request.form['query'], file=sys.stderr)
     resp = make_response(render_template('querypage.html'),200)
     resp.text = "I got it dawg!"
-    return resp
+    return request.form['query']
