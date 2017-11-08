6dd25eac9a0b3db089ca5034b4fc06ec59894473
benstoehr <benjaminxstoehr@gmail.com>
2017-11-08 16:16:11 -0600

    Condition.py

--- server/query/Condition.py
+++ server/query/Condition.py
@@ -1,5 +1,8 @@
+import re
+import numpy
+
 class Condition:
-    def __init__(self, first, second, first_op=lambda x: x, second_op=lambda x: x):
+    def __init__(self, first, second):
         self.first = first
         self.second = second
         self.first_op = first_op
@@ -11,7 +14,66 @@ class Condition:
     def get_second(self):
         return self.second
 
-    def apply(self, val1, val2):
-        val1 = self.first_op(val1)
-        val2 = self.second_op(val2)
+class ConditionLessThan(Condition):
+    def __init__(self, first, second):
+        Condition.__init__(self, first, second)
+
+    def apply(val1, val2):
+        if(numpy.isnan(val1) || numpy.isnan(val2)):
+            return False;
         return val1 < val2
+
+class ConditionGreaterThan(Condition):
+    def __init__(self, first, second):
+        Condition.__init__(self, first, second)
+
+    def apply(val1, val2):
+        if(numpy.isnan(val1) || numpy.isnan(val2)):
+            return False;
+        return val1 > val2
+
+class ConditionGreaterThanOrEqual(Condition):
+    def __init__(self, first, second):
+        Condition.__init__(self, first, second)
+
+    def apply(val1, val2):
+        if(numpy.isnan(val1) || numpy.isnan(val2)):
+            return False;
+        return val1 >= val2
+
+class ConditionLessThanOrEqual(Condition):
+    def __init__(self, first, second):
+        Condition.__init__(self, first, second)
+
+    def apply(val1, val2):
+        if(numpy.isnan(val1) || numpy.isnan(val2)):
+            return False;
+        return val1 <= val2
+
+class ConditionEqual(Condition):
+    def __init__(self, first, second):
+        Condition.__init__(self, first, second)
+
+    def apply(val1, val2):
+        if(numpy.isnan(val1) || numpy.isnan(val2)):
+            return False;
+        return val1 == val2
+
+class ConditionNotEqual(Condition):
+    def __init__(self, first, second):
+        Condition.__init__(self, first, second)
+
+    def apply(val1, val2):
+        if(numpy.isnan(val1) || numpy.isnan(val2)):
+            return False;
+        return val1 != val2
+
+class ConditionLike(Condition):
+    def __init__(self, first, second):
+        Condition.__init__(self, first, second)
+
+    def apply(val1, val2):
+        if(numpy.isnan(val1) || numpy.isnan(val2)):
+            return False;
+        pattern = re.compile(val1)
+        return pattern.match(val2)
