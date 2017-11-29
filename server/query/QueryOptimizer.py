d58161f2f288a52d79f11097460e29a21964a345
Jacob Trueb <trueb2@illinois.edu>
2017-11-29 11:36:22 -0600

    Added QueryPlan and pseudo code to QueryOptimizer

--- server/query/QueryOptimizer.py
+++ server/query/QueryOptimizer.py
@@ -1,10 +1,19 @@
+from server.query.QueryPlan import QueryPlan
+
+
 class QueryOptimizer:
     """
     With the output from the QueryParser, create a plan for executing the query efficiently.
+
+        `WHERE NOT (A.a < B.a OR A.a > B.a)`
+        should translate to
+        `WHERE NOT (A.a <> B.a)
+        should translate to
+        `WHERE A.a = B.a`
     """
 
     def __init__(self, parsed_query):
-        self.parsed_query = parsed_query
+        self._parsed_query = parsed_query
         pass
 
     def get_plan(self):
@@ -12,17 +21,63 @@ class QueryOptimizer:
         Returns a plan to execute using the QueryFacade
         :return:
         """
+        plan = QueryPlan()
+
         # TODO: handle JOIN with conditions
+        join_plan = self._reduce_joins()
 
-        # TODO: find comparisons between column and constant
-
-        # TODO: find comparisons between column and column
+        # TODO: get the comparisons between (columns and constants) and (columns and columns)
+        where_plan = self._get_where_conditions()
 
         # TODO: logically change comparisons (ie NOT A.a < A.b should be A.a >= A.b)
+        # this is complicated
+        reduced_plan = self._reduce_where_plan()
 
-        # TODO: return Comparison objects in list form or something
+        return reduced_plan
 
-        return None
+    def _reduce_joins(self):
+        """
+        Remove the JOIN ON stuff from the FROM clause using conditions and stuff
+
+        :return: a QueryPlan with the conditions for the JOIN ON
+        """
+        plan = QueryPlan()
+        from_tables = self._parsed_query.from_tables
+        for join_on in from_tables:
+            table1 = join_on.first_table
+            table2 = join_on.second_table
+            # TODO: this is all pseudo code
+            for condition in join_on.conditions:
+                args = (table1, condition.column1), (table2, condition.column2), condition.comparison, condition.logic
+                plan.add_column_comparison(*args)
+        return plan
+
+    def _get_where_conditions(self):
+        plan = QueryPlan()
+
+        # TODO: find comparisons between column and constant
+        # use this to add column and constant comparisons
+        table1, table2 = 'Table1', 'Table2'
+        condition = {} # IDK what conditions will look like out of the parser
+        constant = "HelloWorld"
+        args = (table1, condition.column1), QueryOptimizer.try_parse_constant(constant), condition.comparison, condition.logic
+        plan.add_column_comparison(*args)
+        # TODO: find comparisons between column and column
+        # use this to add column and column comparisons
+        args = (table1, condition.column1), (table2, condition.column2), condition.comparison, condition.logic
+        plan.add_column_comparison(*args)
+
+        # TODO: IDK how the parenthesis handle sub queries and conjuctions and stuff
+        where_conditions = self._parsed_query.where_conditions
+        conjunctions = self._parsed_query.conjunctions
+        for condition in where_conditions:
+            pass
+
+        return plan
+
+    def _reduce_where_plan(self, where_plan):
+        plan = QueryPlan()
+        return plan
 
     def get_data_sources(self):
         """
@@ -33,3 +88,14 @@ class QueryOptimizer:
 
         # TODO: return objects that describe the from table (make an object or something)
         pass
+
+    @staticmethod
+    def try_parse_constant(val):
+        # TODO: Strip outer quotes
+        try:
+            return float(val)
+        except ValueError:
+            try:
+                return int(val)
+            except ValueError:
+                return val
---
+++ server/query/QueryPlan.py
@@ -0,0 +1,84 @@
+class QueryPlan:
+    """
+    A QueryPlan.py is constructed by the QueryOptimizer
+
+    It contains information about where data sources are,
+    what comparisons need to be performed, the order to perform
+    the comparisons, and what columns will be projected in the result.
+
+    A QueryPlan.py can contain another QueryPlan.py as a condition in order to
+    support queries like WHERE A.a = B.a AND (A.b < B.b OR A.c > B.c)
+
+    Sometimes NOTs are not optimized by the QueryOptimizer, ie
+    `WHERE NOT A.a < B.a` translates to `WHERE A.a >= B.a` but
+
+    `WHERE NOT ((A.a < B.a OR A.a > B.a) AND A.b = B.b)` translates to
+    `WHERE NOT (A.a <> B.a AND A.b = B.b)
+    """
+
+    def __init__(self):
+        # The conditions that will be executed in this plan
+        self._conditions = []
+
+        # The way that conditions are related (AND, OR)
+        self._conjunctions = []
+
+        # Flag for NOT preceding this query plan
+        self._is_negated = []
+
+    def add_column_comparison(self, column1, column2, comparison, conjunction=None):
+        """
+        Adds a condition to the query plan
+        :param column1: first column in the comparison
+        :param column2: second column in the comparison
+        :param comparison: the comparison to make
+        :param conjunction: AND, OR logic for combining results of condition
+        :return:
+        """
+        self._add_condition((column1, column2, comparison), conjunction)
+
+    def add_query_plan_condition(self, sub_query_plan, conjunction=None):
+        """
+        Adds conditions that are in another QueryPlan.py
+        The conditions in parenthesis are represented by the sub_query_plan
+        ie WHERE A.a = B.b AND (A.b = B.c OR A.c = B.d)
+        :param sub_query_plan: QueryPlan.py to add to this one
+        :param conjunction: AND, OR logic for combining results of condition
+        :return:
+        """
+        self._add_condition(sub_query_plan, conjunction)
+
+    def _add_condition(self, condition, conjunction):
+        """
+        Private method adds condition with conjunction
+        :param condition:
+        :param conjunction:
+        :return: None
+        """
+        self._conditions.append(condition)
+        if conjunction is not None:
+            self._conjunctions.append(conjunction)
+
+        assert len(self._conditions) == len(self._conjunctions) + 1, "The number of conditions does not match the " \
+                                                                     "number of conjunctions."
+
+    @property
+    def data_sources(self):
+        return self._data_sources
+
+    @property
+    def conditions(self):
+        return self._conditions
+
+    @property
+    def conjunctions(self):
+        return self._conjuctions
+
+    @property
+    def is_negated(self):
+        return self._is_negated
+
+    @is_negated.setter
+    def is_negated(self, value):
+        assert isinstance(value, bool), "The is_negated flag must be a boolean."
+        self._is_negated = value
