8d6ab7b36897090230b80f440aae0376428a591d
Jacob Trueb <trueb2@illinois.edu>
2017-12-02 19:02:42 -0600

    Added DISTINCT support for the TableProjector

--- server/query/Column.py
+++ server/query/Column.py
@@ -6,7 +6,12 @@ class Column:
     def __init__(self, table, name):
         assert isinstance(table, Table), "A Column must belong to a Table."
         self._name = name
+        self._table = table
 
     @property
     def name(self):
         return self._name
+
+    @property
+    def table(self):
+        return self._table
---
+++ server/query/test/table_projector_test.py
@@ -0,0 +1,20 @@
+from server.query.Column import Column
+from server.query.Table import Table
+from server.query.TableProjector import TableProjector
+
+if __name__ == "__main__":
+    t1 = Table("./data/movies.csv")
+    t2 = Table("./data/movies.csv")
+    tables = [t1, t2]
+    projection_columns = [Column(t1, 'movie_title'), Column(t2, 'director_name')]
+    tp = TableProjector(tables, projection_columns)
+    output = tp.aggregate([(419, 419), (710, 710), (1055, 1055)], False)
+    assert output == ['Avatar,James Cameron', "Pirates of the Caribbean: At World's End,Gore Verbinski", 'Spectre,Sam Mendes']
+
+    output = tp.aggregate([(419, 419), (710, 710), (1055, 1055), (419, 419)], False)
+    assert output == ['Avatar,James Cameron', "Pirates of the Caribbean: At World's End,Gore Verbinski", 'Spectre,Sam Mendes', 'Avatar,James Cameron']
+
+    output = tp.aggregate([(419, 419), (710, 710), (1055, 1055), (419, 419)], True)
+    # Using DISTINCT changes the order of the output rows because of sets
+    assert set(output) == set(['Avatar,James Cameron', "Pirates of the Caribbean: At World's End,Gore Verbinski", 'Spectre,Sam Mendes'])
+
