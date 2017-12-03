e379b7b314b8d611cf38e5fca4f2990a19658e25
Jacob Trueb <trueb2@illinois.edu>
2017-12-02 18:37:03 -0600

    Handle Parenthesis in Comparisons

--- server/query/test/parser_test.py
+++ server/query/test/parser_test.py
@@ -33,7 +33,23 @@ if __name__ == "__main__":
     assert str(conds[0][1]) == 'release_year < 2010'
     assert str(conds[1][0]) == 'release_year = 2011'
 
+    query = 'SELECT movie_title FROM movies WHERE movie_title = "Spider-Man 3" AND (release_year < 2010 OR release_year = 2011)'
+    p = Parser(query)
+    cols, tbls, conds = p.parse_select_from_where()
+    assert cols == ['movie_title']
+    assert tbls == ['movies']
+    assert str(conds[0][0]) == 'movie_title = "Spider-Man 3"'
+    assert str(conds[0][1][0][0]) == 'release_year < 2010'
+    assert str(conds[0][1][1][0]) == 'release_year = 2011'
 
+    query = 'SELECT movie_title FROM movies WHERE (movie_title = "Spider-Man 3" AND (release_year < 2010 OR release_year = 2011))'
+    p = Parser(query)
+    cols, tbls, conds = p.parse_select_from_where()
+    assert cols == ['movie_title']
+    assert tbls == ['movies']
+    assert str(conds[0][0][0][0]) == 'movie_title = "Spider-Man 3"'
+    assert str(conds[0][0][0][1][0][0]) == 'release_year < 2010'
+    assert str(conds[0][0][0][1][1][0]) == 'release_year = 2011'
 
 
 
